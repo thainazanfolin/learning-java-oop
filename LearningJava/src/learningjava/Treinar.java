69d0a1fc667095e533ea21f94b2896ff64bb8d2e
Thainá R Zanfolin <102308025+thainazanfolin@users.noreply.github.com>
2023-02-28 07:42:12 -0300

    treinando atributos, get e set nas classes

--- LearningJava/src/learningjava/Treinar.java
+++ LearningJava/src/learningjava/Treinar.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package learningjava;
 
 
 public class Treinar {
     
     /* atributos */ 
+   String nome;
+   String modalidade;
+   int tempoDeDuração;
+   int caloriasGastas;
+   int frequenciaPorSemana;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
+
+    public int getTempoDeDuração() {
+        return tempoDeDuração;
+    }
+
+    public int getCaloriasGastas() {
+        return caloriasGastas;
+    }
+
+    public int getFrequenciaPorSemana() {
+        return frequenciaPorSemana;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setModalidade(String modalidade) {
+        this.modalidade = modalidade;
+    }
+
+    public void setTempoDeDuração(int tempoDeDuração) {
+        this.tempoDeDuração = tempoDeDuração;
+    }
+
+    public void setCaloriasGastas(int caloriasGastas) {
+        this.caloriasGastas = caloriasGastas;
+    }
+
+    public void setFrequenciaPorSemana(int frequenciaPorSemana) {
+        this.frequenciaPorSemana = frequenciaPorSemana;
+    }
    
         
 }
