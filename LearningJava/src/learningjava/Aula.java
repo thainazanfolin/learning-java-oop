660fc1e1fea43e116f353a32ce062e789f50e7cd
Thainá R Zanfolin <102308025+thainazanfolin@users.noreply.github.com>
2023-02-28 07:40:12 -0300

    treinando atriutos outras classes

--- LearningJava/src/learningjava/Aula.java
+++ LearningJava/src/learningjava/Aula.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package learningjava;
 
-/**
- *
- * @author Usuario
- */
 public class Aula {
     
+    String nome;
+    String nomeDoProfesor;
+    int duracao;
+    int semestre;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNomeDoProfesor() {
+        return nomeDoProfesor;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNomeDoProfesor(String nomeDoProfesor) {
+        this.nomeDoProfesor = nomeDoProfesor;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
 }
+
+    
+            
