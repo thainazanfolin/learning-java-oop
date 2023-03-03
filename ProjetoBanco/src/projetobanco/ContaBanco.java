7a09d4deaa71ba8595ad3c8184eb27a01f0b48b7
Thainá R Zanfolin <102308025+thainazanfolin@users.noreply.github.com>
2023-03-03 09:40:59 -0300

    adc atributos, getters e setters

--- ProjetoBanco/src/projetobanco/ContaBanco.java
+++ ProjetoBanco/src/projetobanco/ContaBanco.java
@@ -6,5 +6,56 @@ package projetobanco;
 
 public class ContaBanco {
     
-   
+   //atributos
+    
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private double saldo;
+    private boolean status;
+    
+   //gets
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+    
+    //setters
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
 }
