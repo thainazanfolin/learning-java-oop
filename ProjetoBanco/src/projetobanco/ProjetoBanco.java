02d5d0110240f001a2f471ad7a9ff671be5c07f0
Thainá R Zanfolin <102308025+thainazanfolin@users.noreply.github.com>
2023-03-07 09:35:48 -0300

    instanciando objetos na main

--- ProjetoBanco/src/projetobanco/ProjetoBanco.java
+++ ProjetoBanco/src/projetobanco/ProjetoBanco.java
@@ -8,7 +8,22 @@ public class ProjetoBanco {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        ContaBanco conta1 = new ContaBanco();   //instanciando objeto sem parametros no construtor
+        conta1.setNumConta(111);
+        conta1.setDono("José");
+        conta1.setStatus(true);
+        conta1.abrirConta("cc");
+    
+        conta1.statusAtual();       // mostrando as config atuais do objeto instanciado
         
+        
+        ContaBanco conta2 = new ContaBanco();
+        conta2.setNumConta(112);
+        conta2.setDono("Vitor");
+        conta2.setStatus(true);
+        conta2.abrirConta("cp");
+        
+        conta2.statusAtual();
         
     }
     
